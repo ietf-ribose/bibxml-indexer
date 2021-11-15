8c301e66d29fccd44339f5a9265fd00928dcae04
Anton Strogonoff <anton@strogonoff.name>
2021-11-15 20:25:40 +0100

    chore(models): synchronize model definition

--- api/models.py
+++ api/models.py
@@ -2,13 +2,26 @@ from django.db import models
 
 
 class RefData(models.Model):
+    ref = models.CharField(
+        max_length=128,
+        help_text="Reference (or ID). Corresponds to source dataset filename without extension.")
+
     ref_id = models.CharField(max_length=64)
+    """DEPRECATED: Use ref"""
+
     ref_type = models.CharField(max_length=24)
+    """DEPRECATED: Use ref"""
 
-    # Matches ID in settings.RELATON_DATASETS
-    dataset = models.CharField(max_length=24)
+    dataset = models.CharField(
+        max_length=24,
+        help_text="Internal dataset ID.")
+    """Matches ID in settings.RELATON_DATASETS."""
 
-    body = models.JSONField('body')
+    body = models.JSONField()
+    """Contains canonical Relaton representation"""
+
+    representations = models.JSONField()
+    """Contains a mapping of { format: string }, where format is e.g. bibxml"""
 
     class Meta:
         db_table = 'api_ref_data'
