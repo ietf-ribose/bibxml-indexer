b3572b43b60403357c2b649c1b6b650709d24841
Anton Strogonoff <anton@strogonoff.name>
2021-11-17 12:19:27 +0100

    fix: exempt POST API endpoints from CSRF

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 
@@ -6,19 +7,19 @@ urlpatterns = [
     path('v1/', views.index, name='api_index'),
 
     re_path(r'^v1/indexer/(?P<dataset_name>[\w-]+)/run',
-            views.api_run_indexer,
+            csrf_exempt(views.api_run_indexer),
             name='api_run_indexer'),
     re_path(r'^v1/indexer/(?P<dataset_name>[\w-]+)/reset',
-            views.api_reset_index,
+            csrf_exempt(views.api_reset_index),
             name='api_reset_index'),
     re_path(r'^v1/indexer/(?P<dataset_name>[\w-]+)/status',
             views.api_indexer_status,
             name='api_indexer_status'),
 
     re_path(r'^v1/tasks/(?P<task_id>[\w-]+)/stop',
-            views.api_stop_task,
+            csrf_exempt(views.api_stop_task),
             name='api_stop_task'),
     re_path(r'^v1/tasks/stop-all',
-            views.api_stop_all_tasks,
+            csrf_exempt(views.api_stop_all_tasks),
             name='api_stop_all_tasks'),
 ]
